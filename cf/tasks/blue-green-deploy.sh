936b5d6bb8a538afbd0bd8f72f6e259dd987ab34
Rejo R Thekkudan <rejo.thekkudan@cgius.com>
2018-08-22 15:12:48 -0500

    Blue green deploy

--- cf/tasks/blue-green-deploy.sh
+++ cf/tasks/blue-green-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 set -x
 
 version=`cat version/version`
@@ -42,7 +43,7 @@ echo "apps_url: shows the apps assigned to the hostname: ${apps_url}"
 
 # TO DO: Finish the jq parsing
 # Fetch the app names assigned to the hostname
-app_names=`(cf curl $apps_url | jq -r '...')`
+app_names=`(cf curl $apps_url | jq -r '.resources[].entity | .name')`
 
 for name in $app_names; do
     if [ "$name" != "$app_name" ]
